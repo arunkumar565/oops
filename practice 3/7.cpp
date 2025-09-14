118bfd9aaf3ba373ceb79c77873dbc439e798dfb
arunkumar2005coder <arunhp2005@outlook.com>
2025-09-14 23:44:51 +0530

    Updated codes in practice 3 (7.cpp, 8.cpp, 9.cpp modified)

--- practice 3/7.cpp
+++ practice 3/7.cpp
@@ -1,5 +1,37 @@
-#include<iostream>
-#include<string>
+#include <iostream>
 using namespace std;
-class {};
-int main(){};
+
+class Circle {
+private:
+    double radius;   
+public:
+
+    void setRadius(double r) {
+        radius = r;
+    }
+
+    double getRadius() {
+        return radius;
+    }
+
+    
+    double getArea() {
+        double r = getRadius();      
+        return 3.14159 * r * r;     
+    }
+};
+
+int main() {
+    Circle c1;
+    double r;
+
+    cout << "Enter the radius of the circle: ";
+    cin >> r;
+
+    c1.setRadius(r);
+
+    cout << "Radius: " << c1.getRadius() << endl;
+    cout << "Area: " << c1.getArea() << endl;
+
+    return 0;
+}
